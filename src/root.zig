b0d2cf5c1710c1c68234d3ccaaf5cb39c262e762
Justin Heyes-Jones <justinhj@gmail.com>
2025-11-03 08:58:15 -0800

    Work on parsing full tweets

--- src/root.zig
+++ src/root.zig
@@ -13,6 +13,7 @@ const std = @import("std");
 
 // Sample post from tweets.js
 
+// window.YTD.tweets.part0 = [
 // {
 //   "tweet" : {
 //     "edit_info" : {
@@ -65,6 +66,23 @@ const std = @import("std");
 //   }
 // },
 
+const Tweet = struct {
+    tweet : TweetTweet,
+};
+
+const TweetTweet = struct {
+    retweeted: bool,
+    source: []const u8,
+    favorite_count: []const u8,
+    full_text: []const u8,
+    id_str : []const u8,
+    possibly_sensitive : bool,
+    created_at : []const u8,
+    favorited : bool,
+    in_reply_to_screen_name : []const u8,
+    in_reply_to_user_id_str : []const u8
+};
+
 const TweetHeader = struct {
     tweet : TweetHeaderTweet
 };
@@ -109,7 +127,7 @@ pub fn parseTweetHeaders(allocator: std.mem.Allocator, buffer: []const u8) Tweet
 
 const testing = std.testing;
 
-test "Parse test" {
+test "Parse headers" {
     const sample_content =
       \\window.YTD.tweet_headers.part0 = [
       \\  {
@@ -134,4 +152,85 @@ test "Parse test" {
     const result = try parseTweetHeaders(testing.allocator, sample_content);
     defer testing.allocator.free(result);
     try testing.expectEqualDeep(result, &parsed);
+}
+
+test "Parse tweets" {
+    const sample_content =
+      \\ window.YTD.tweets.part0 = [
+      \\ {
+      \\   "tweet" : {
+      \\     "edit_info" : {
+      \\       "initial" : {
+      \\         "editTweetIds" : [
+      \\           "1977039067859894552"
+      \\         ],
+      \\         "editableUntil" : "2025-10-11T16:50:20.000Z",
+      \\         "editsRemaining" : "5",
+      \\         "isEditEligible" : false
+      \\       }
+      \\     },
+      \\     "retweeted" : false,
+      \\     "source" : "<a href=\"https:\\mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      \\     "entities" : {
+      \\       "hashtags" : [ ],
+      \\       "symbols" : [ ],
+      \\       "user_mentions" : [ ],
+      \\       "urls" : [
+      \\         {
+      \\           "url" : "https:\\t.co/RwFBCwuHS5",
+      \\           "expanded_url" : "https:\\codeberg.org/ziglings/exercises",
+      \\           "display_url" : "codeberg.org/ziglings/exerc…",
+      \\           "indices" : [
+      \\             "0",
+      \\             "23"
+      \\           ]
+      \\         }
+      \\       ]
+      \\     },
+      \\     "display_text_range" : [
+      \\       "0",
+      \\       "23"
+      \\     ],
+      \\     "favorite_count" : "0",
+      \\     "in_reply_to_status_id_str" : "1977039066895204461",
+      \\     "id_str" : "1977039067859894552",
+      \\     "in_reply_to_user_id" : "29532976",
+      \\     "truncated" : false,
+      \\     "retweet_count" : "0",
+      \\     "id" : "1977039067859894552",
+      \\     "in_reply_to_status_id" : "1977039066895204461",
+      \\     "possibly_sensitive" : false,
+      \\     "created_at" : "Sat Oct 11 15:50:20 +0000 2025",
+      \\     "favorited" : false,
+      \\     "full_text" : "https:\\t.co/RwFBCwuHS5",
+      \\     "lang" : "zxx",
+      \\     "in_reply_to_screen_name" : "justinhj",
+      \\     "in_reply_to_user_id_str" : "29532976"
+      \\   }
+      \\   }]
+          ;
+
+    const sample_tweet = Tweet {
+        .tweet = TweetTweet {
+            .retweeted = false,
+            .source = "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+            .favorite_count = "0",
+            .full_text = "https://t.co/RwFBCwuHS5",
+            .id_str = "1977039067859894552",
+            .possibly_sensitive = false,
+            .created_at = "Sat Oct 11 15:50:20 +0000 2025",
+            .favorited = false,
+            .in_reply_to_screen_name = "justinhj",
+            .in_reply_to_user_id_str = "29532976"
+        }
+    };
+
+    try testing.expect(sample_content.len > 0);
+    try testing.expect(sample_tweet.tweet.retweeted == false);
+
+    // const parsed: [1]TweetHeader = .{sample_tweet};
+
+    // const result = try parseTweetHeaders(testing.allocator, sample_content);
+    // defer testing.allocator.free(result);
+    // try testing.expectEqualDeep(result, &parsed);
 }
